b975266b4fab5bf860925d2dfd86bdda5c92d21a
Andrea Lo Iacono <andrea.loiacono.wrk@gmail.com>
2025-05-27 20:47:11 +0200

    Fized index.js and prepare deploy

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,48 +1,44 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cittadinoRoutes = require('./routes/cittadinoRoute');
-const comuneRoutes = require('./routes/comuneRoute');
-const operatoreRoutes = require('./routes/operatoreRoute');
+const express = require("express");
+const mongoose = require("mongoose");
+const cittadinoRoutes = require("./routes/cittadinoRoute");
+const comuneRoutes = require("./routes/comuneRoute");
+const operatoreRoutes = require("./routes/operatoreRoute");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const cors = require("cors");
 const app = express();
 const dbURI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
 
 //Middleware to parse JSON data
 app.use(express.json());
 app.use(
-    cors({
-      origin: "http://localhost:8081",
-      credentials: true,
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
-  
-// Import routes
-app.use('/api/v1/cittadino', cittadinoRoutes);
-app.use('/api/v1/comune', comuneRoutes);
-app.use('/api/v1/operatoreComunale', operatoreRoutes);
-
-mongoose.connect(dbURI)
-    .then(() =>
-    { 
-        console.log("Connected to DB!")
-        app.listen(3000, () => 
-        {
-            console.log("Server is running on port 3000 ")
-        });
-            
-    })
-    .catch((err) =>
-    {
-        console.log(err)
-    })
-
-
-app.get('/', (req, res) => 
-{
-  res.send('Hello World!');
-}
+  cors({
+    origin: "http://localhost:8081",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
+
+// Import routes
+app.use("/api/v1/cittadino", cittadinoRoutes);
+app.use("/api/v1/comune", comuneRoutes);
+app.use("/api/v1/operatoreComunale", operatoreRoutes);
+
+
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("Connected to DB!");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
