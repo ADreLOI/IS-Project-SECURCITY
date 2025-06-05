9df761bd767ca69aa10096491b9f5196c01dc5c9
Matthew De Marco <matthew.demarco17@gmail.com>
2025-06-05 23:24:24 +0200

    FourSquare API implemented

---
+++ FrontEnd/MobileApp/app/screens/(tabs)/home/getBusiness.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const getBusiness = () => {
+  return (
+    <View>
+      <Text>getBusiness</Text>
+    </View>
+  )
+}
+
+export default getBusiness
+
+const styles = StyleSheet.create({})
